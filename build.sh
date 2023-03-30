dfd97ee0521e0b8ee0551cec40281027e64b7962
Lars Nieuwenhuizen <l.nieuwenhuizen@developers.nl>
2023-03-30 12:18:52 +0200

    feat(ci): Build the images with the released tag if any

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 
-set -eux pipefail
+set -eux
+
+tag=${1:-'no-tag'}
 
 platforms="linux/amd64,linux/arm64"
 
-docker buildx build --no-cache --platform=$platforms --target=install -t larsnieuwenhuizen/ci-helpers:latest --push .
+
+if [[ $tag = 'no-tag' ]]; then
+  docker buildx build --no-cache --platform=$platforms --target=install -t larsnieuwenhuizen/ci-helpers:latest --push .
+else
+  docker buildx build --no-cache --platform=$platforms --target=install -t larsnieuwenhuizen/ci-helpers:${tag} -t larsnieuwenhuizen/ci-helpers:latest --push .
+fi
