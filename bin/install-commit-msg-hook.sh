fc8a09235e935127a232c92d09aa840a333da47c
Lars Nieuwenhuizen <l.nieuwenhuizen@connectingthedots.nl>
2023-03-30 10:25:33 +0200

    fix: The installed git hook binary was not executable

--- bin/install-commit-msg-hook.sh
+++ bin/install-commit-msg-hook.sh
@@ -4,7 +4,8 @@ set -eux
 
 ## note: Download and place the commit message hook
 wget https://raw.githubusercontent.com/LarsNieuwenhuizen/ci-helpers/main/git-hooks/commit-msg;
-mv commit-msg .git/hooks/commit-msg
+mv commit-msg .git/hooks/commit-msg;
+chmod +x .git/hooks/commit-msg;
 
 # note: Download the json config file the hook uses
 wget https://raw.githubusercontent.com/LarsNieuwenhuizen/ci-helpers/main/git-hooks/commit-msg.config.json
